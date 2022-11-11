81219c0954aa647325b8a45ef6d0037a6e3b934b
Farrukh Atabekov <augini@inbox.ru>
2022-11-12 00:43:13 +0500

    feat: add italy

---
+++ frames/wine.py
@@ -0,0 +1,142 @@
+wine_frames = [
+    """                          
+    
+    
+                                                                                                                                         
+            &#GPYJ77!~~^^^::^~~!?J5GB&                         &#GPYJ77!~~^^^::^~~!?J5GB&           
+          Y~:.                       .^?B                   &Y~:.                       .^?B        
+         &J~.                          :J                   &J~.                          :Y        
+        &~?PGP5YJ7!!~~~~~~~~~!!7?JJY5PGPY~#                &~?PGP5YJ7!!~~~~~~~~~!!7?JJY5PGPY~&      
+        G    :^!7JJYYYYYYYYYYYJJ?7!~~^.   J                P   .:^!7JJYYYYYYYYYYYJJ?7!~^^.   Y      
+        Y                                 ^                J                                 ~      
+        ?                                 .#               7                                 :&     
+        ?                                  G               7                                 .#     
+        7                                  G               !                                  B     
+        ?                                  B               7                                 .#     
+        5                                 .#               Y                                 :&     
+        B                                 ~                G                                 ~      
+         ^                                J                 ^                            .:^~P      
+         5....   . ........::::^^^~~!!7?JY#                 G7!~^^::::.::::^^^^~!!!7??JY55PGG       
+          GP55555555555PPPPPPPPGGGGGGGGGGB                   BGGGGPPPPPPPPPGGGGGGGGGGGGGGGGPB       
+          &GGGGGGGGGGGGGGGGGGGGGGGGGGGGGP&                   &GPGGGGGGGGGGGGGGGGGGGGGGGGGGPG&       
+           #PPGGGGGGGGGGGGGGGGGGGGGGGGGP#                     #PPGGGGGGGGGGGGGGGGGGGGGGGGGP#        
+            #PPGGGGGGGGGGGGGGGGGGGGGGPPB                       #PPGGGGGGGGGGGGGGGGGGGGGGPP#         
+             &GPGGGGGGGGGGGGGGGGGGGGPG#                         #GPGGGGGGGGGGGGGGGGGGGGPG#          
+              &BPPGGGGGGGGGGGGGGGGPPB&                           &BPPGGGGGGGGGGGGGGGGPPB&           
+                &GPPPGGGGGGGGGGPPPG#                               #GPPPGGGGGGGGGGPPPG#             
+                  &BGPPPPPPPPPPGB#                                   &BGPPPPPPPPPPGB#               
+                     &#BGGGBBB#&                                        &#BGGGBBB&                  
+                          BG                                                 GB                     
+                          !~                                                 ~!                     
+                          7~                                                 !!                     
+                          7~                                                 !!                     
+                          7!                                                 !7                     
+                          7?                                                 !J                     
+                          !J                                                 ~Y                     
+                          ~J                                                 ^Y                     
+                          ^J                                                 :Y                     
+                          :J                                                &:Y                     
+                         &.J                                                #.Y                     
+                         # ?                                                B J                     
+                         5 !                                                5 7                     
+                         ! ^                                                ~ ^                     
+                &BPY?!! G   G 55G#                                 &BPY?!7 P   B 55G#               
+              P7:     5 ^   ^BG: .^!JG                           P!:     P&:   ^#P. .^!JG           
+             5       .5!     .~.      ^&                        Y       :5~     .~.      ~&         
+             &Y~:                   :!5                         &J~.                   :!P          
+                &BPYJ?7777777??JY5GB&                              &BPYJ?7777777??JY5GB&        
+    """,
+    """ 
+    
+                                                                                 
+                   &&##BBBB####&                                        &&&&&&&&                    
+                 J^:...    ....:^^~7J5G#&                     &#GP5J7!~^::...:::^~!7JP#             
+               &#P!.                   .:!JP#             B5?~:.                      ^&            
+               ? 7PGPJ!^.                   .~Y&        G^                      .:^!JPBYJ           
+              G    .~7YPPP5YJ7!~^::....      .:G        BY?!^::......::^~~7?JY5PPP5Y?~.  P          
+              !         .:^!7?JY55PPPPPPPPPPPPPJ#      #^!?Y5PPPPPPPPP55YYJ?7~^:.        ^          
+             G                      .....:::..  G      B        ...                       P         
+             ?                                  P      #.                                 ~         
+            &:                                  B      &:                                 .#        
+            G                                  :&       ~                                  G        
+            5                                  7        J                                  5        
+            Y                                  G        G                                  5        
+            Y                            .:^!?5          5?!^:.                            5        
+            P                     ..:~!?Y5PGGG&          &GGGP5YJ7~^:..                  .:G        
+            #........:::^^~~!!7?JY5PPGGGGGGGP#            BPGGGGGGGGPP5YYJ??77!!!!77??JY5PP&        
+             P5555PPPPPPGGGGGGGGGGGGGGGGGGGPB              GPGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG         
+             #GGGGGGGGGGGGGGGGGGGGGGGGGGGGPG&              &GPGGGGGGGGGGGGGGGGGGGGGGGGGGGP#         
+              BPGGGGGGGGGGGGGGGGGGGGGGGGGPG&                &GPGGGGGGGGGGGGGGGGGGGGGGGGGPB          
+              &GPGGGGGGGGGGGGGGGGGGGGGGPPG&                  &GPPGGGGGGGGGGGGGGGGGGGGGGPG           
+               &GPGGGGGGGGGGGGGGGGGGGPPG#                      BGPGGGGGGGGGGGGGGGGGGGGPB            
+                 BPPGGGGGGGGGGGGGGGPPGB&                        &BGPGGGGGGGGGGGGGGGGPPB             
+                  #GPPGGGGGGGGGGPPPGB&                            &BGPPPGGGGGGGGGPPPG&              
+                    #BGPPPPPPPPGGB#                                 &#BGPPPPPPPPPGB&                
+                      &#B#BBB##&                                       &&#BBBBB##&                  
+                         5P                                                   YB                    
+                        &.5                                                   ^7                    
+                        5.&                                                   J^                    
+                        ~7                                                    G.#                   
+                       #.G                                                    &:P                   
+                       J!                                                      ~Y                   
+                      &^P                                                      ?7                   
+                      5:&                                                      Y:                   
+                      ^?                                                       G.B                  
+                     G B                                                       #.5                  
+                     ~^                                                        &:7                  
+                    P ?                                                         ^:&                 
+                   #: P                                                         ^ P                 
+         #BP5YYJJ&&~  B                                                        &: ^  &&             
+       Y~.      5&~   G J5B                                              #PJ!~&G   J 5^^~7JPB&      
+      &^       JG^    !&J  :7P&                                       G7^    7 !    !P7      :5     
+       &Y!.            ::     .J                                     7       7J             .!B     
+          &GY?!^:..           .!&                                    Y^.              .:^!JP#       
+                &&#BGPP55YY5PG&                                        #GPYJJJJJY5PGB##&  
+    """,
+    """            
+                                                                      
+                     &BGPPPPGGB##&&                                                                 
+                     ?         ..:^~7?YPB&                       &#BG5J??777777?JJ5B&               
+                   5JBP!.                :~?P#             &B5J!~:.                 7               
+                  P  .7PGPJ~:                .~JG      &P?~.                    :~?PBJJ             
+                 #:     .~?5PP5J7~^.             ^P   G:                .:^!?Y5PP5J!.  5            
+                 ~           :~7?Y5PPP5YJ?7!~^^^^^J   B5Y?7!!~~~!77?JY5PPP5Y?!^:       .B           
+                5                   .:^!7?JYY55P55?G  ~^!?JYYYYYYJ?7!~^:.               !           
+               #:                                  P  ~                                  G          
+               J                                  .#  J                                  !          
+              &^                                  !   P                                  .#         
+              G                                   P   &:                                  G         
+              Y                                  ~     ?                                  Y         
+              ?                                 :B     #:                                 J         
+              ?                        ..:^~!?JYB       BY?7~^:.                        .:Y         
+              P?777!!!!!!!!~!!!!77??JYY5PPGGGGG#         BGGGGPP5YJ?7!!~^^^^^^^^^~!!7JY5PP#         
+              &GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGPB          &GPGGGGGGGGGGGGGGGGGGGGGGGGGGGGGP#         
+               GPGGGGGGGGGGGGGGGGGGGGGGGGGGGPB            &GPGGGGGGGGGGGGGGGGGGGGGGGGGGGPG&         
+               #PGGGGGGGGGGGGGGGGGGGGGGGGGPPB              &GPGGGGGGGGGGGGGGGGGGGGGGGGGGPB          
+                BPGGGGGGGGGGGGGGGGGGGGGGGPG#                &GPPGGGGGGGGGGGGGGGGGGGGGGGPG           
+                 GPGGGGGGGGGGGGGGGGGGGPPGB&                   #GPGGGGGGGGGGGGGGGGGGGGGPG&           
+                 &GPPGGGGGGGGGGGGGGGPPGB&                      &BGPPGGGGGGGGGGGGGGGGPPG&            
+                   #GPPGGGGGGGGGPPPPG#&                          &BGPPPGGGGGGGGGGGPPGB              
+                    &#GGPPPPPPPGGB#&                               &#GGPPPPPPPPPPPGB&               
+                       &##BBB##&                                      &#BBGGGGGBB#&                 
+                        P?                                                  &&&P&                   
+                       &:?                                                     ^?                   
+                       J:&                                                     Y:&                  
+                      #.P                                                      #.5                  
+                      77                                                        7!                  
+                     P^&                                                        G:&                 
+                    &^5                                                          ^P                 
+                    7~                                                           7!                 
+                   G.G                                                           P.#                
+                  &:7                                                            &:J                
+                  7.#                                                             7^                
+                 J ?                                                              5 5               
+      #BBGGGGB  J  B                                                              G ^               
+   B7^.     .5&7  ^                                                               G  J  &&&         
+   Y       :BB^   ^ P~JG                                                     &GY? 5   5 Y^:^!7J5B   
+    P!.    .^      JP.  ^J#                                               BJ~.  ! 7    !P7       Y  
+      &P?~:                ?&                                           B~      7P.            ^J#  
+          &BG5J7~^:.     .:!&                                           ?                .:~?5B     
+                  &##BBGB#&                                              GY?7!~~~!!7JY5GB#  
+    """,
+]
--- oldik.py
+++ oldik.py
@@ -2,7 +2,14 @@ import typer
 
 import time
 
-from frames import piyola_frames, beer_frames, glass_frames, cola_frames, broken_glass_frames
+from frames import (
+    piyola_frames,
+    beer_frames,
+    glass_frames,
+    cola_frames,
+    broken_glass_frames,
+    wine_frames,
+)
 
 DEFAULT_FRAMES = glass_frames
 
@@ -10,13 +17,14 @@ VERSION_TO_FRAMES = {
     "uzbekistan": piyola_frames,
     "germany": beer_frames,
     "canada": broken_glass_frames,
-    "cola": cola_frames
+    "cola": cola_frames,
+    "italy": wine_frames,
 }
 
 
 def clear_line(n=1):
-    LINE_UP = '\033[1A'
-    LINE_CLEAR = '\x1b[2K'
+    LINE_UP = "\033[1A"
+    LINE_CLEAR = "\x1b[2K"
     for i in range(n):
         print(LINE_UP, end=LINE_CLEAR)
 
@@ -27,7 +35,7 @@ def animate_frames(frames):
         line_count = len(frames)
         frame = frames[i]
         print(frame)
-        time.sleep(.2)
+        time.sleep(0.2)
         clear_line(len(frame.split("\n")))
         i += 1
         i %= line_count
@@ -42,5 +50,5 @@ def main(version: str = ""):
     animate_version(version)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     typer.run(main)

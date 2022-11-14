64bef78766d0977f1b1f8cda0537b05894e4cdd3
Farrukh Atabekov <augini@inbox.ru>
2022-11-14 13:01:20 +0500

    feat: sync with the origin

--- frames/__init__.py
+++ frames/__init__.py
@@ -4,4 +4,6 @@ from .glass import glass_frames
 from .cola import cola_frames
 from .broken_glass import broken_glass_frames
 from .wine import wine_frames
+
+from .love import love_frames
 from .frames import get_ascii_frames
--- oldik.py
+++ oldik.py
@@ -1,6 +1,12 @@
 import typer
+
 import time
+
+
+from frames import piyola_frames, beer_frames, glass_frames, cola_frames, broken_glass_frames, love_frames
+
 from frames import piyola_frames, beer_frames, glass_frames, cola_frames, broken_glass_frames, wine_frames, get_ascii_frames
+
 
 DEFAULT_FRAMES = glass_frames
 
@@ -9,13 +15,14 @@ VERSION_TO_FRAMES = {
     "germany": beer_frames,
     "canada": broken_glass_frames,
     "cola": cola_frames,
+    "love": love_frames,
     "italy": wine_frames,
 }
 
 
 def clear_line(n=1):
-    LINE_UP = "\033[1A"
-    LINE_CLEAR = "\x1b[2K"
+    LINE_UP = '\033[1A'
+    LINE_CLEAR = '\x1b[2K'
     for i in range(n):
         print(LINE_UP, end=LINE_CLEAR)
 
@@ -39,17 +46,18 @@ def animate_version(version: str = ""):
     frames = VERSION_TO_FRAMES.get(version.lower(), DEFAULT_FRAMES)
     animate_frames(frames)
 
-# animate_gif converts gif file to frames and animates as ascii art
+
 def animate_gif(path: str):
     frames = get_ascii_frames(path)
     animate_frames(frames)
+
 
 def main(version: str = "uzbekistan", path: str = None):
     # If gif path specified, then animate gif
     if path:
         animate_gif(path)
-        return
-    animate_version(version)
+    else:
+        animate_version(version)
 
 
 if __name__ == '__main__':
